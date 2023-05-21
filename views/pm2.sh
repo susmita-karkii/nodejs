333df3bf6a7fb9e637cfd3e9c03d1ffa485516a0
susmita-karkii <85597124+susmita-karkii@users.noreply.github.com>
2023-05-21 22:54:56 +0545

    Delete pm2.sh

--- views/pm2.sh
+++
@@ -1,8 +0,0 @@
-if pm2 show app >/dev/null; then
-    echo "Restarting PM2 process for app.js"
-    pm2 stop app.js
-    pm2 restart app.js
-else
-    echo "Starting PM2 process for app.js"
-    pm2 start app.js
-fi

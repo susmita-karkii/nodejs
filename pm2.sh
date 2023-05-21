a4831fed7b4f7aac906833bde20646d48cfac7b6
susmita-karkii <85597124+susmita-karkii@users.noreply.github.com>
2023-05-21 22:56:28 +0545

    Update pm2.sh

--- pm2.sh
+++ pm2.sh
@@ -1,1 +1,8 @@
-
+if pm2 show app >/dev/null; then
+    echo "Restarting PM2 process for app.js"
+    pm2 stop app.js
+    pm2 restart app.js
+else
+    echo "Starting PM2 process for app.js"
+    pm2 start app.js
+fi
